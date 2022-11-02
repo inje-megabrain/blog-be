10eb0bff028f039c8239103251e8ceabe789f927
mjh000526 <mjh000526@gmail.com>
2022-11-02 19:53:50 +0900

    삭제

--- blog-server/comment_update/src/main/java/com/example/cb/dao/CommentDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.cb.dao;
-
-import com.example.cb.entity.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentDao extends CrudRepository<Comment, String> {
-
-
-}
