201b9ec072b927cacc6e84e7dbffefff3dc33adc
MoonSangWon <kuim75@naver.com>
2022-11-15 20:01:55 +0900

    젠킨스 테스트

--- blog-server/blog/src/main/java/com/example/cb/config/WebConfig.java
+++ blog-server/blog/src/main/java/com/example/cb/config/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET","POST");
-        
     }
 
 }
